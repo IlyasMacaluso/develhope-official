3465b93af3a5e0b98af07f307a480bf50da2dd7d
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-04-04 11:48:43 +0200

    esercizio 6 comletato

--- esercizi-DIY/esercizi-stefan/es-06-mattina(da-finire).js
+++ esercizi-DIY/esercizi-stefan/es-06-mattina(da-finire).js
@@ -20,9 +20,38 @@ tipologie :
 **
 *
 */
-let i = "**"
-for (let n = 4; n > 0; n--) {
-    i = i + "*"
-    console.log(i);
+
+function fromN(n) {
+    let string = "*".repeat(n)
+    for (let i = 0; i < n; i++) {
+        console.log(string)
+        string = string.substr(1)
+    }
 }
 
+function toN(n) {
+    let string = "*"
+    for (let i = 0; i < n; i++) {
+        console.log(string)
+        string = string.concat("*")
+    }
+}
+
+function fromNtoN(n) {
+    let string = "*".repeat(n)
+    for (let i = -n + 1; i <= n - 1; i++) {
+        if (i < 0) {
+            console.log(string)
+            string = string.substr(1)
+        } else {
+            console.log(string)
+            string = string.concat("*")
+        }
+    }
+}
+
+toN(6)
+
+fromN(6)
+
+fromNtoN(6)
