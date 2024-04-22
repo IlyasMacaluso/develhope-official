2e6baba639adbc860bbd5185e00f6528ae7f5216
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-04-22 15:40:24 +0200

    esecizio conditional rendering 1

---
+++ corso/react-esercizi/my-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { Welcome } from "./Welcome";
+
+export function App () {
+    return (
+        <div>
+            <h1>La mia App</h1>
+            <Welcome name="jhon"age={18} />
+        </div>
+    )
+}
---
+++ corso/react-esercizi/my-app/src/Hello.jsx
@@ -0,0 +1,9 @@
+import { Message } from "./Message"
+export function Hello() {
+    return (
+        <div>
+            <Message />
+            <h2>Hello World!</h2>
+        </div>
+    )
+}
