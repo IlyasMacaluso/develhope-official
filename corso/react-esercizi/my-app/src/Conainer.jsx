b0c684914af75c117dbddcf07e40b37763d093a2
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-04-30 17:48:38 +0200

    fix commento

--- corso/react-esercizi/my-app/src/Conainer.jsx
+++ corso/react-esercizi/my-app/src/Conainer.jsx
@@ -4,14 +4,14 @@ import ContainerChildren from "./ContainerChildren"
 
 function Container({ title, children }) {
     const [collapsed, setCollapsed] = useState(true)
-    
+
     function handleContentToggle () {
         setCollapsed(b => !b)
     }
     return (
         <div className={styles.container}>
             <div className="container-title">{title}</div>
-           {/*  da chiedere perchè non funziona sul componente direttamente
+           {/*  da chiedere perchè la classe non funziona sul componente direttamente
             <ContainerChildren className={collapsed ? styles.containerContent : styles.containerContentHidden} /> */}
             <div className={collapsed ? styles.containerContent : styles.containerContentHidden}>{children}</div>
             <button type="button" onClick={handleContentToggle}>Show/Hide Cotnent</button>
