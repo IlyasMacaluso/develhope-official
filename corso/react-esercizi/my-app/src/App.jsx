0be6a757ce4b19d54f50625dea4bfa43f25dc3cf
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-06 15:37:35 +0200

    esercizio reactRouter 3

--- corso/react-esercizi/my-app/src/App.jsx
+++ corso/react-esercizi/my-app/src/App.jsx
@@ -1,81 +1,10 @@
-import { Welcome } from "./Welcome"
-import { AlertClock } from "./AlertClock"
-import { Counter } from "./Counter"
-import { CounterUseEff } from "./CounterUseEff"
-import { MouseClicker } from "./MouseClicker"
-import { InteractiveWelcome } from "./InteractiveWelcome"
-import { Login } from "./Login"
-import { UncontrolledLogin } from "./UncontrolledLogin"
-import FocusableInput from "./FocusableInput"
-import MountedRef from "./MountedRef"
-import CounterRef from "./CounterRef"
-import Colors from "./Colors"
-import ToDoList from "./ToDoList"
-import Container from "./Conainer"
-import ClockWrapper from "./ClockWrapper"
-import GitHubUser from "./GitHubUser"
-// import GitHubUsers from "./GitHubUsers"
-import CustomHookLogin from "./CustomHookLogin"
-import GitHubUsersCustomHooks from "./GitHubUsersCustomHook"
-import Ripasso from "./Ripasso"
-import CurrentLocation from "./CurrentLocation"
 import GitHubUsers from "./ReactRouter3/GitHubUsers"
 import { Route, Routes } from "react-router-dom"
 
-const colors = [
-    { id: 1, name: 'green' },
-    { id: 2, name: 'red' },
-    { id: 3, name: 'blue' },
-    { id: 4, name: 'yellow' },
-    { id: 5, name: 'purple' },
-]
 export function App() {
-    function onLogin(data) {
-        console.log(data)
-    }
     return (
         <div className="app">
             <h1>La mia App</h1>
-            <hr />
-            <Welcome name="jhon" age={19} />
-            <AlertClock />
-            <hr />
-            <Counter initialValue={0} incrementAmount={5} />
-            <hr />
-            <CounterUseEff initialValue={10} incrementAmount={10} />
-            <hr />
-            <ClockWrapper />
-            <hr />
-            <MouseClicker label="Click me!" />
-            <hr />
-            <InteractiveWelcome />
-            <hr />
-            <Login onLogin={onLogin} />
-            <hr />
-            <UncontrolledLogin />
-            <hr />
-            <FocusableInput />
-            <hr />
-            <MountedRef />
-            <hr />
-            <CounterRef initialValue={0} incrementAmount={10} />
-            <hr />
-            <Colors colors={colors} />
-            <hr />
-            <ToDoList />
-            <hr />
-            <Container/>
-            <hr />
-            <GitHubUsers/>
-            <hr />
-            <CustomHookLogin />
-            <hr />
-            <GitHubUsersCustomHooks />
-            <hr />
-            <Ripasso />
-            <hr />
-            <CurrentLocation />
-            <hr />
             <Routes >
                 <Route path=":id" element= {<GitHubUsers /> } />
             </Routes>
--- corso/react-esercizi/my-app/src/index.jsx
+++ corso/react-esercizi/my-app/src/index.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client"
 import  Root  from "./Root"
 import { StrictMode } from "react"
 import "./index.css"
-import "./Clock.scss"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById("root")).render(
