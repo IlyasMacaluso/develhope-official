93e43c0b35c0bb490b3c041a61604b1328f840f9
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-06 16:02:16 +0200

    esercizio React Router 5

--- corso/react-esercizi/my-app/src/App.jsx
+++ corso/react-esercizi/my-app/src/App.jsx
@@ -3,6 +3,7 @@ import Counter from "./Counter"
 import GitHubUsers from "./GitHubUsers"
 
 import { Link, Route, Routes } from "react-router-dom"
+import NotFound from "./NotFound"
 
 export function App() {
     return (
@@ -11,13 +12,14 @@ export function App() {
             <hr />
             <div>
                 <Link to="/">Counter</Link> | <Link to="counter">Counter</Link> |
-                <Link to="/2">User with id 2</Link>
+                <Link to="/user/2">User with id 2</Link>
             </div>
 
             <Routes>
                 <Route path="/" element={<Welcome name="Ilyas" />} />
                 <Route path="counter" element={<Counter />} />
-                <Route path=":id" element={<GitHubUsers />} />
+                <Route path="user/:id" element={<GitHubUsers />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     )
