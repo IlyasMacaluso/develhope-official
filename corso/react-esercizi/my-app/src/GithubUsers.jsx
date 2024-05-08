e22a6eb166c6885ccec7883281052529c53e175f
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-08 15:16:04 +0200

    setup in progress

--- corso/react-esercizi/my-app/src/GithubUsers.jsx
+++ corso/react-esercizi/my-app/src/GithubUsers.jsx
@@ -1,34 +1,46 @@
-import { useEffect, useState } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { useState } from "react";
+import { useGitHubUser } from "./useGithubUser";
+import GitHubUser from "./GitHubUser";
 
-function GithubUsers() {
-    const [data, setData] = useState([])
+function GithubUsersCustomHooks () {
+    const [username, setUsername] = useState("")
+    const {data, error, loading } = useGithubUser()
 
-    async function fetchData() {
-        try {
-            const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
-            const json = await res.json()
-            setData(json)
-        } catch (error) {
-            console.error(error.message)
-        }
+    function handleUsernameChange(e) {
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        setUsername(formData.get("username"))
+        e.target.reset()
     }
-    useEffect(() => {
-        fetchData()
-    }, [])
+
     return (
-        <ul>
-            {data &&
-                data.map((user, index) => {
-                    return (
-                        <li key={index}>
-                            <Link to={`${user.id}`}>{user.name}</Link>
-                        </li>
-                    )
-                })}
-            <Outlet />
-        </ul>
-    )
+        <>
+            <h1>GithubUsers (search)</h1>
+            <ul className="results">
+                {loading && <h2>Loading...</h2>}
+                {error && <h2>{error}</h2>}
+                {username &&
+                    data &&
+                    username !== "" &&
+                    data
+                        .filter((user) => user.name.toUpperCase().includes(username.toUpperCase()))
+                        .map((user, index) => (
+                            <>
+                                <GithubUser
+                                    key={index}
+                                    username={user.username}
+                                    name={user.name}
+                                />
+                            </>
+                        ))}
+            </ul>
+
+            <form action="#" onSubmit={handleUsernameChange}>
+                <input name="username" type="text" placeholder="Insert username here" />
+                <button type="submit">Search User</button>
+            </form>
+        </>)
 }
 
-export default GithubUsers
+export default GithubUsersCustomHooks
---
+++ corso/react-esercizi/my-app/src/useGithubUser.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+export function useGithubUser() {
+
+    const [data, setData] = useState (null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    // nota: sto usando una API differente perchè quella di Github veniva bloccata dopo una decina di tentativi bloccandomi l'accesso per ore
+    async function fetchData() {
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users")
+            if (res.status !== 200) {
+                setError(new Error("Fetch request failed"))
+            } else {
+                const data = await res.json()
+                setData(data)
+                console.log(data)
+            }
+        } catch (error) {
+            setError(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect (() => {
+        fetchData()
+    }, [])
+
+    return {data, error, loading}
+}
