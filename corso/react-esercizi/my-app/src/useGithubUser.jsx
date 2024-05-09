eaec34e25256a38fe86f483da028bbbd3e3d68a1
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-05-09 11:18:57 +0200

    esercizio completato

--- corso/react-esercizi/my-app/src/useGithubUser.jsx
+++ corso/react-esercizi/my-app/src/useGithubUser.jsx
@@ -2,7 +2,9 @@ import useSWR from "swr"
 
 export function useGithubUser(username) {
     console.log("Loading data...")
-    const { data, error, mutate } = useSWR(`https://api.github.com/users/${username}`)
+    const { data, error, mutate } = useSWR(
+        username ? `https://api.github.com/users/${username}` : null
+    )
     return {
         data,
         error,
